af57a38179c199797996ba4a80e3f9716f82da84
FluxIndustries <88303769+FluxIndustries@users.noreply.github.com>
2021-10-30 21:01:59 +0200

    🖍️ Changed blob to edit for the edit this page links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/FluxIndustries/fluxdocs/blob/master/',
+          editUrl: 'https://github.com/FluxIndustries/fluxdocs/edit/master/',
         },
         // blog: {
         //   showReadingTime: true,
